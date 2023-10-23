e55a1c3f0f274359221eb69dd6a1cf02658ebf07
anuj-as <anujsaxena628@gmail.com>
2023-10-23 22:19:22 +0530

    Print
    In the given code on editor, modify the Java print statement to print “I learnt tricks”.

---
+++ Intro-to-Java/Print.java
@@ -0,0 +1,7 @@
+public class Print { //kindly change the class name to HelloWorld
+    public static void main(String[] args){
+
+        //Modify this statement
+        System.out.println("I learnt tricks");
+    }
+}
