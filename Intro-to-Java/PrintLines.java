290b9d6c18aa273d97483d6cc2b8441e357a3eca
anuj-as <anujsaxena628@gmail.com>
2023-10-23 22:21:51 +0530

    Print Lines
    Write the code snippet inside main method to print the following:                                                             I’m learning Java.
    Java is an object-oriented programming language.

---
+++ Intro-to-Java/PrintLines.java
@@ -0,0 +1,7 @@
+public class PrintLines { // kindly change the class name to SampleProgram
+    public static void main(String[] args){
+        //Write your code below
+        System.out.println("I'm learning Java.");
+        System.out.println("Java is an object-oriented programming language.");
+    }    
+}
