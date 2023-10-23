663b497500ee31fdfb34f258655c3b4757cace93
anuj-as <anujsaxena628@gmail.com>
2023-10-23 22:23:14 +0530

    some modification done

---
+++ Intro-to-Java/CorrectTheCode.java
@@ -0,0 +1,6 @@
+public class CorrectTheCode {// kindly change the class name to Solution
+    public static void main(String[] args){
+        System.out.println("You have successfully removed the error");
+    }
+    
+}
--- Intro-to-Java/RemoveError.java
+++ Intro-to-Java/RemoveError.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class RemoveError {//Kindly change the class name to HelloWorld
     public static void  main(String[] args){
         System.out.println("You have successfully removed the main method error");
     }
