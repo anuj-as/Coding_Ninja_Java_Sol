52ebb9d139e04ae75675b216919500849cdb7321
anuj-as <anujsaxena628@gmail.com>
2023-10-23 22:46:22 +0530

    Printing Multiple Lines
    Write the Java code to display the given lines as it is on screen using the knowledge of print statements taught in the above lessons.             In the realm of atoms and galaxies,
    Where mysteries unfurl like cosmic seas,
    Science, our guiding star so bright,
    Reveals the universe, its boundless light.
    
    From microcosms to celestial grace,
    Inquiring minds explore every space,
    In the quest for knowledge, we stand tall,
    Science, the greatest adventure of all.

---
+++ Intro-to-Java/Assignment/PrintingMultipleLines.java
@@ -0,0 +1,17 @@
+public class PrintingMultipleLines {// kindly change the class name to SciencePoem
+    public static void main(String[] args) {
+        //Write your code here
+	    //Pay attention to the new line and the ending characters.
+        System.out.println("In the realm of atoms and galaxies,\r\n" + //
+                "Where mysteries unfurl like cosmic seas,\r\n" + //
+                "Science, our guiding star so bright,\r\n" + //
+                "Reveals the universe, its boundless light.\r\n" + //
+                "\r\n" + //
+                "From microcosms to celestial grace,\r\n" + //
+                "Inquiring minds explore every space,\r\n" + //
+                "In the quest for knowledge, we stand tall,\r\n" + //
+                "Science, the greatest adventure of all.");
+
+        
+    }
+}
